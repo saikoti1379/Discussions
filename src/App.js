3d65e31969088e11dd04736e200d40ae0fc751f1
sai koti <saikoti1379@gmaol.com>
2023-05-29 07:28:43 +0000

    why it is shoowing error

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from './components/UserProfile/index'
+
+const UserDetails = {
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'koti',
+  role: 'Software',
+}
+
+const App = () => <UserProfile />
+
+export default App
